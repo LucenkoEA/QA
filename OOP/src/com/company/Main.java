c7b42efbc4b29fa8a20b3cf73a9a1c61aabcd39d
Eugene Lutsenko <lucenko_e@icloud.com>
2021-11-12 19:05:44 +0300

    Add new files

---
+++ OOP/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Phone IPhone11 = new Phone();
+        IPhone11.receiveCall("Евгений");
+    }
+}
---
+++ OOP/src/com/company/Phone.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Phone {
+    public long number = 89189764535L;
+    public int model = 11;
+    public int weight = 20;
+    public void receiveCall(String name) {
+        System.out.println("Звонит " + name);
+    }
+    }
+
