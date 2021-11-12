e85772e53ebbbc6c8dcf08f91aa08b016a40be55
Eugene Lutsenko <lucenko_e@icloud.com>
2021-11-12 19:16:39 +0300

    Add modified files

--- OOP/src/com/company/Main.java
+++ OOP/src/com/company/Main.java
@@ -3,7 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    Phone IPhone11 = new Phone();
-        IPhone11.receiveCall("Евгений");
+        Phone Phone1 = new Phone (111, 236, 300);
+        Phone1.receiveCall("Евгений", 89189764535L);
+        System.out.println("Номер телефона: " + Phone1.number);
+        System.out.println("Модель телефона: " + Phone1.model);
+        System.out.println("Вес телефона: " + Phone1.weight);
+
+        Phone Phone2 = new Phone (222, 166, 350);
+        Phone1.receiveCall("Денис", 89891256489L);
+        System.out.println("Номер телефона: " + Phone2.number);
+        System.out.println("Модель телефона: " + Phone2.model);
+        System.out.println("Вес телефона: " + Phone2.weight);
+
+        Phone Phone3 = new Phone(333, 211, 250);
+        Phone1.receiveCall("Владислав", 89286547125L);
+        System.out.println("Номер телефона: " + Phone3.number);
+        System.out.println("Модель телефона: " + Phone3.model);
+        System.out.println("Вес телефона: " + Phone3.weight);
     }
 }
+
--- OOP/src/com/company/Phone.java
+++ OOP/src/com/company/Phone.java
@@ -1,11 +1,24 @@
 package com.company;
 
 public class Phone {
-    public long number = 89189764535L;
-    public int model = 11;
-    public int weight = 20;
-    public void receiveCall(String name) {
-        System.out.println("Звонит " + name);
+    public int number;
+    public int model;
+    public int weight;
+
+    public void receiveCall (String name) {
+        System.out.print("Звонит " + name + " ");
     }
+    public void getNumber (long num) {
+        System.out.println(num);
     }
+    public Phone (int n, int m, int w) {
+        number = n;
+        model = m;
+        weight = w;
+    }
+    public void receiveCall (String name, long num) {
+        System.out.println("Звонит " + name + " " + num);
+    }
+}
+
 
